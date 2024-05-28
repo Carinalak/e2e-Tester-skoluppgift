17834bf9976c4f7da79e0adf9b5257cc6cb00ff1
Carinalak <carina.lakosil@gmail.com>
2024-05-28 08:27:18 +0200

    search movie test

--- cypress/e2e/functions.cy.ts
+++ cypress/e2e/functions.cy.ts
@@ -1,11 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Movie search and sort', () => {
+  it('searches for movies and sorts results', () => {
     // Assign
+    cy.visit("http://localhost:5173");
 
     // Act
+    cy.get("input#searchText").type("ghost");
+    cy.get("button#search").click();
 
     //Assert
+    cy.get('#movie-container')
+    .should("have.length", 1)
+    //.should("have.text", "ghost");
+  });
+  
 
-    
-  })
-})
+});
+
