ca79e29a1fac6943d84eb0f0409b8c4098bf3b3a
Carinalak <carina.lakosil@gmail.com>
2024-05-28 14:36:18 +0200

    sort test

--- cypress/e2e/functions.cy.ts
+++ cypress/e2e/functions.cy.ts
@@ -12,7 +12,37 @@ describe('Movie search and sort', () => {
     .should("have.length", 1)
     //.should("have.text", "ghost");
   });
-  
+
+
+  it('should sort movies correctly after searching', () => {
+    // Assign
+    cy.visit("http://localhost:5173");
+
+    // Act
+    cy.get("input#searchText").type("ghost");
+    cy.get("button#search").click();
+    
+    // Assert - ascending order
+    cy.get('#movie-container').then(titles => {
+      const sortedTitles = [...titles]
+        .map(el => el.innerText)
+        .sort();
+      expect([...titles]
+        .map(el => el.innerText)).to.deep
+        .equal(sortedTitles);
+    });
+    
+    // Assert - descending order
+    cy.get('#movie-container').then(titles => {
+      const sortedTitles = [...titles]
+        .map(el => el.innerText)
+        .sort().reverse();
+      expect([...titles]
+        .map(el => el.innerText)).to.deep
+        .equal(sortedTitles);
+    });
+    
+  });
 
 });
 
---
+++ cypress/e2e/movieApp.cy.ts
@@ -0,0 +1,20 @@
+describe('template spec', () => {
+  it('passes', () => {
+    // Assign
+    cy.visit('https://example.cypress.io')
+
+
+    // Act
+
+
+
+    // Assert
+
+
+
+    
+  })
+
+
+})
+
